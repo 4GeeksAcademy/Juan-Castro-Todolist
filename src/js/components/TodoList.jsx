2ce6b37e4b5e5951db17864e3e235097accd3e5f
juan <130606010+juancastrop@users.noreply.github.com>
2025-10-26 18:43:48 +0000

    reparacion de colores en textos

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -43,7 +43,7 @@ const TodoList = () => {
                                     onClick={() => removeTodo(i)}
                                     aria-label={`Eliminar ${t}`}
                                 >
-                                    X
+                                    Eliminar
                                 </button>
                             </li>
                         ))}
